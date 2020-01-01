acff8cf94e46e4d5fcbd6453abd50c2838f40aca
amuk1989 <58622904+amuk1989@users.noreply.github.com>
2020-01-01 23:19:49 +0700

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+
+            char sym2 = '#';
+
+            draw(x2, y2, sym2);
+
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(sym2);
+
+            Console.ReadLine();
+        }
+
+        static void draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
