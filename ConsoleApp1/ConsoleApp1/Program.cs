74dc67d6d46e1ca149bb675bd6890e913f583013
Andrew Muk <amuk89@mail.ru>
2020-01-02 23:16:15 +0700

    list

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,7 +19,22 @@ namespace ConsoleApp1
             Point p2 = new Point(4,5,'#');
             p2.draw();
 
+            List<int> numList = new List<int>();
 
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
 
             Console.ReadLine();
         }
